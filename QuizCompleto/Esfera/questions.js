d27a7bcd755ccae448b84f1bd152cdf43be9bb45
enryson <enryson@gmail.com>
2019-06-10 19:40:24 -0300

    fix esfera

--- QuizCompleto/Esfera/questions.js
+++ QuizCompleto/Esfera/questions.js
@@ -10,7 +10,7 @@ var questions = [
     ["  . (Enem 2ª aplicação 2016)  A bocha é um esporte jogado em canchas, que são terrenos planos e nivelados, limitados por tablados perimétricos de madeira. O objetivo desse esporte é lançar bochas, que são bolas feitas de um material sintético, de maneira a situá-las o mais perto possível do bolim, que é uma bola menor feita, preferencialmente, de aço, previamente lançada.  A Figura 1 ilustra uma bocha e um bolim que foram jogados em uma cancha. Suponha que um jogador tenha lançado uma bocha, de raio 5cm que tenha ficado encostada no bolim, de raio 2cm conforme ilustra a Figura 2.Considere o ponto C como o centro da bocha, e o ponto O como o centro do bolim. Sabe-se que A e B são os pontos em que a bocha e o bolim, respectivamente, tocam o chão da cancha, e que a distância entre  A e B é igual a  Nessas condições, qual a razão entre  d e o raio do bolim? ",
 
         "a)1", " ", "", "d)2", "",
-        "img/dois.png", "", "img/2 b.png", "img/2 c.png", "", "img/2 e.png",
+        "img/dois.png", "", "img/2b.png", "img/2c.png", "", "img/2e.png",
         "5"
     ],
     [". (Enem 2014)  Uma empresa farmacêutica produz medicamentos em pílulas, cada uma na forma de um cilindro com uma semiesfera com o mesmo raio do cilindro em cada uma de suas extremidades. Essas pílulas são moldadas por uma máquina programada para que os cilindros tenham sempre 10mm de comprimento, adequando o raio de acordo com o volume desejado.Um medicamento é produzido em pílulas com 5mm de raio. Para facilitar a deglutição, deseja-se produzir esse medicamento diminuindo o raio para   e, por consequência, seu volume. Isso exige a reprogramação da máquina que produz essas pílulas.Use 3 como valor aproximado para PI A redução do volume da pílula, em milímetros cúbicos, após a reprogramação da máquina, será igual a ",
@@ -30,21 +30,21 @@ var questions = [
     ["(Enem PPL 2014)  A figura é uma representação tridimensional da molécula do hexafluoreto de enxofre, que tem a forma bipiramidal quadrada, na qual o átomo central de enxofre está cercado por seis átomos de flúor, situados nos seis vértices de um octaedro. O ângulo entre qualquer par de ligações enxofre-flúor adjacentes mede 90 graus  A vista superior da molécula, como representada na figura, é:  ",
 
         "", "", "", "", "",
-        "img/cinco.png", "img/5 a.png", "img/5 b.png", "img/5 c.png", "img/5 d.png", "img/5 e.png",
+        "img/cinco.png", "img/5a.png", "img/5b.png", "img/5c.png", "img/5d.png", "img/5e.png",
         "2"
     ],
 
     ["(Enem 2012)  O globo da morte é uma atração muito usada em circos. Ele consiste em uma espécie de jaula em forma de uma superfície esférica feita de aço, onde motoqueiros andam com suas motos por dentro. A seguir, tem-se, na Figura 1, uma foto de um globo da morte e, na Figura 2, uma esfera que ilustra um globo da morte.Na Figura 2, o ponto A está no plano do chão onde está colocado o globo da morte e o segmento AB passa pelo centro da esfera e é perpendicular ao plano do chão. Suponha que há um foco de luz direcionado para o chão colocado no ponto B e que um motoqueiro faça um trajeto dentro da esfera, percorrendo uma circunferência que passa pelos pontos A e B.A imagem do trajeto feito pelo motoqueiro no plano do chão é melhor representada por  ",
 
         "", "", "", "", "",
-        "img/seis.png", "img/6 a.png", "img/6 b.png", "img/6 d.png", "img/6 d1.png", "img/6 e.png",
+        "img/seis.png", "img/6a.png", "img/6b.png", "img/6d.png", "img/6d1.png", "img/6e.png",
         "5"
     ],
 
     ["(Enem 2ª aplicação 2010)  Se pudéssemos reunir em esferas toda a água do planeta, os diâmetros delas seriam:A razão entre o volume da esfera que corresponde à água doce superficial e o volume da esfera que corresponde à água doce do planeta é  ",
 
         "", "", "", "", "",
-        "img/sete.png", "img/7 a.png", "img/7 b.png", "img/7 c.png", "img/7 d.png", "img/7 e.png",
+        "img/sete.png", "img/7a.png", "img/7b.png", "img/7c.png", "img/7d.png", "img/7e.png",
         "1"
     ],
 
@@ -58,7 +58,7 @@ var questions = [
     ["(Enem cancelado 2009)  Um artista plástico construiu, com certa quantidade de massa modeladora, um cilindro circular reto cujo diâmetro da base mede   e cuja altura mede   Antes que a massa secasse, ele resolveu transformar aquele cilindro em uma esfera.  Analisando as características das figuras geométricas envolvidas, conclui-se que o raio   da esfera assim construída é igual a Volume da esfera:",
 
         "", "", "", "", "",
-        "img/nove.png", "img/a.png", "img/9 b.png", "img/9 c.png", "img/9 d.png", "img/9 e.png",
+        "img/nove.png", "img/a.png", "img/9b.png", "img/9c.png", "img/9d.png", "img/9e.png",
         "4"
     ],
 
