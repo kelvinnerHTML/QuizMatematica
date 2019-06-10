4a7cb73cd97e85903b62c826201d7af21df7b77f
enryson <enryson@gmail.com>
2019-06-10 19:46:45 -0300

    fix fq

--- QuizCompleto/FQ/questions.js
+++ QuizCompleto/FQ/questions.js
@@ -1,80 +1,79 @@
 var questions = [
 
     ["",
-        "", "", "", "","",
-        "imgs/0ex1.png", "imgs/1alt1.png", "imgs/1alt2.png", "imgs/1alt3.png", "imgs/1alt4.png","imgs/1alt5.png",
+        "", "", "", "", "",
+        "img/0ex1.png", "img/1alt1.png", "img/1alt2.png", "img/1alt3.png", "img/1alt4.png", "img/1alt5.png",
         "4"
     ],
     ["",
         "", "", "", "", "",
-        "imgs/0ex2.png", "imgs/2ctrl1.png", "imgs/2ctrl2.png", "imgs/2ctrl3.png", "imgs/2ctrl4.png","imgs/2ctrl5.png",
+        "img/0ex2.png", "img/2ctrl1.png", "img/2ctrl2.png", "img/2ctrl3.png", "img/2ctrl4.png", "img/2ctrl5.png",
         "4"
     ],
 
     ["",
-        "", "", "", "","",
-        "imgs/0ex3.png", "imgs/3shift1.png", "imgs/3shift2.png", "imgs/3shift3.png", "imgs/3shift4.png","imgs/3shift5.png",
-         "3"
+        "", "", "", "", "",
+        "img/0ex3.png", "img/3shift1.png", "img/3shift2.png", "img/3shift3.png", "img/3shift4.png", "img/3shift5.png",
+        "3"
 
 
     ],
 
     ["",
-        "", "", "", "","",
-        "imgs/0ex4.png", "imgs/4enter1.png", "imgs/4enter2.png", "imgs/4enter3.png", "imgs/4enter4.png","imgs/4enter5.png",
-         "4"
+        "", "", "", "", "",
+        "img/0ex4.png", "img/4enter1.png", "img/4enter2.png", "img/4enter3.png", "img/4enter4.png", "img/4enter5.png",
+        "4"
 
 
     ],
 
     ["",
-        "", "", "", "","",
-        "imgs/0ex5.png", "imgs/5windows1.png", "imgs/5windows2.png", "imgs/5windows3.png", "imgs/5windows4.png","imgs/5windows5.png",
-         "1"
+        "", "", "", "", "",
+        "img/0ex5.png", "img/5windows1.png", "img/5windows2.png", "img/5windows3.png", "img/5windows4.png", "img/5windows5.png",
+        "1"
 
 
     ],
 
     ["",
-        "", "", "", "","",
-        "imgs/0ex6.png", "imgs/6esc1.png", "imgs/6esc2.png", "imgs/6esc3.png", "imgs/6esc4.png","imgs/6esc5.png",
-         "4"
+        "", "", "", "", "",
+        "img/0ex6.png", "img/6esc1.png", "img/6esc2.png", "img/6esc3.png", "img/6esc4.png", "img/6esc5.png",
+        "4"
 
 
     ],
 
     ["",
-        "", "", "", "","",
-        "imgs/0ex7.png", "imgs/7delete1.png", "imgs/7delete2.png", "imgs/7delete3.png", "imgs/7delete4.png","imgs/7delete5.png",
-         "5"
+        "", "", "", "", "",
+        "img/0ex7.png", "img/7delete1.png", "img/7delete2.png", "img/7delete3.png", "img/7delete4.png", "img/7delete5.png",
+        "5"
 
 
     ],
 
     ["",
-        "", "", "", "","",
-        "imgs/0ex8.png", "imgs/8tab1.png", "imgs/8tab2.png", "imgs/8tab3.png", "imgs/8tab4.png","imgs/8tab5.png",
-         "4"
+        "", "", "", "", "",
+        "img/0ex8.png", "img/8tab1.png", "img/8tab2.png", "img/8tab3.png", "img/8tab4.png", "img/8tab5.png",
+        "4"
 
 
     ],
 
     ["",
-        "", "", "", "","",
-        "imgs/0ex9.png", "imgs/9space1.png", "imgs/9space2.png", "imgs/9space3.png", "imgs/9space4.png","imgs/9space5.png",
-         "4"
+        "", "", "", "", "",
+        "img/0ex9.png", "img/9space1.png", "img/9space2.png", "img/9space3.png", "img/9space4.png", "img/9space5.png",
+        "4"
 
 
     ],
 
     ["",
-        "", "", "", "","",
-        "imgs/0ex10.png", "imgs/10fixa1.png", "imgs/10fixa2.png", "imgs/10fixa3.png", "imgs/10fixa4.png","imgs/10fixa5.png",
-         "2"
+        "", "", "", "", "",
+        "img/0ex10.png", "img/10fixa1.png", "img/10fixa2.png", "img/10fixa3.png", "img/10fixa4.png", "img/10fixa5.png",
+        "2"
 
 
     ],
 
 
 ]
-                                                                        
