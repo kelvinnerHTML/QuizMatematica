772f783fa0f83c2c09049543b75737211e13b78f
kelvinnerHTML <50881538+kelvinnerHTML@users.noreply.github.com>
2019-06-10 14:28:27 -0300

    up

---
+++ Porcentagem1/questions.js
@@ -0,0 +1,71 @@
+var questions = [
+//EX.01
+    ["(Espm 2018) A gravura mostrada na figura abaixo foi dobrada na linha tracejada MN, a x cm da borda AB. Sabendo-se que, depois da dobradura, a parte oculta da gravura representa da parte visível, podemos afirmar que a medida é de",  
+    "3,5 cm", "6 cm", "3 cm", "4,5 cm", "5 cm",
+     "img/e1.png", "", "", "", "", "",
+        "1"
+    ],
+
+//EX.02
+["(Uerj 2018) No mapa mensal de um hospital, foi registrado o total de cirurgias ortopédicas, sendo em homens, conforme os gráficos abaixo. De acordo com esses dados, o número total de cirurgias de fêmur realizadas em mulheres foi: ",  
+"144", "162", "184", "190", "134",
+ "img/e2.png", "", "", "", "", "",
+    "3"
+],
+
+//EX.03
+["(Udesc 2018) A Tabela 1 abaixo representa a tabela nutricional de um determinado tablete de chocolate de 100g. A empresa que produz este chocolate pretende reduzir o tamanho do tablete de 100g para 85g e, para isto, precisará atualizar os valores da Tabela nutricional. Além disso, será incluída uma nova coluna, que conterá os valores diários percentuais de ingestão (VD%) referentes a cada item, com base em uma dieta de 2.000 kcal de acordo com a Tabela 2. Após a atualização da Tabela 1, o percentual do recomendado diário de carboidratos ingeridos em uma porção do novo tablete será equivalente a: ",  
+"2,55%", "3%", "7,65%", "8,5%", "2,83",
+ "img/e3.png", "", "", "", "", "",
+    "1"
+],
+
+//EX.04
+["Upe-ssa 3 2018) A Prefeitura da Cidade Feliz doou um terreno para a Comunidade Viver Bem discutir projetos que deveriam ser implantados no local. Após um planejamento participativo, ficou acertado que 40% da área total desse terreno serão destinados a uma creche; 3% para banheiros públicos e 17% para uma academia de ginástica comunitária. A sobra da área, que é de 800 m2 será utilizada para uma pequena praça com parque de lazer. Qual é a área total, em m2 do terreno doado por essa prefeitura? ",  
+"3.250", "3.000", "2.500", "2.000", "1.750",
+ "", "", "", "", "", "",
+    "4"
+],
+
+//EX.05
+["(Espm 2018) Por volta de 2010, a distribuição da população indígena por região do Brasil era representada pelo gráfico abaixo: Considerando-se que a população indígena total estimada para aquela época era de 325.200, podemos concluir que, na região Sul, o número de indígenas era de aproximadamente",  
+"32.643", "27.967", "19.436", "36.278", "22.308",
+ "", "", "", "", "", "",
+    "2"
+],
+
+//EX.06
+["(Uerj 2018) As farmácias W e Y adquirem determinado produto com igual preço de custo. A farmácia W vende esse produto com 50% de lucro sobre o preço de custo. Na farmácia Y, o preço de venda do produto é 80% mais caro do que na farmácia W. O lucro da farmácia Y em relação ao preço de custo é de:",  
+"170%", "150%", "130%", "110%", "125%",
+ "", "", "", "", "", "",
+    "1"
+],
+
+//EX.07
+["(Udesc 2018)  De forma simplificada, a umidade relativa do ar é calculada pela relação entre a quantidade de vapor de água presente no ar e a quantidade máxima desse vapor no ar, antes que ele fique saturado e a água comece a condensar para a forma líquida, para condições específicas de temperatura e de pressão. Um ambiente fechado de 40 m3 apresenta inicialmente 100% de umidade relativa do ar, com 20g de vapor de água por metro cúbico. Se for colocado neste ambiente um condicionador de ar, que retira do ambiente 1kg de água por hora (de forma constante), e um umidificador, que adiciona 600 g de água por hora (de forma constante), e são ligados simultaneamente, logo a umidade relativa nesse ambiente, após 30 minutos, será de: ",  
+"25%", "100%", "75%", "85%", "15%",
+ "", "", "", "", "", "",
+    "3"
+],
+
+//EX.08
+["(Puccamp 2018)  Segundo dados do IBGE, em 2014 a TV estava presente em 97,1% dos 67 milhões de domicílios brasileiros. De acordo com esse dado, a quantidade de domicílios brasileiros sem TV em 2014 era igual a: ",  
+"650570", "1947000", "6505700", "1943000", "19430000",
+ "", "", "", "", "", "",
+    "4"
+],
+
+//EX.09
+["(Unesp 2017) Uma confeitaria vendeu seus dois últimos bolos por R$ 32,00 cada. Ela teve lucro de 28% com a venda de um dos bolos, e prejuízo de 20% com a venda do outro. No total dessas vendas, a confeitaria teve:",  
+"Prejuízo de R$1,8", "lucro de R$2,56", "prejuízo de R$2,56", "lucro de R$5,12", "prejuízo de R$1,00",
+ "", "", "", "", "", "",
+    "5"
+],
+
+//EX.10
+[". (Pucrj 2017) Abílio tem um salário de R$ 1.000,00 No final do ano, ele recebeu um aumento de 10% devido a uma promoção, seguido, em março, de um reajuste de 5%. Qual o salário de Abílio em abril?",  
+"R$ 1.150,00", "R$ 1.155,00", "R$ 1.105,00", "R$ 1.010,00", "R$ 1.200,00",
+ "", "", "", "", "", "",
+    "2"
+],
+]
