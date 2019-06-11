3b20e705708d6d621292b77d3c4de4e7b470f385
Enryson Oliveira Ferraz <enryson@gmail.com>
2019-06-11 13:17:02 -0300

    fix PC2

--- QuizCompleto/Porcentagem2/questions.js
+++ QuizCompleto/Porcentagem2/questions.js
@@ -1,9 +1,73 @@
 var questions = [
 
-    ["(Uerj simulado 2018)  O tempo necessário para que um planeta do sistema solar execute uma volta completa em torno do Sol é um ano. Observe as informações na tabela:",
-        "73", "76", "79", "82","",
-        "img/ex1.jpg", "", "", "", "","",
-        "2"
+    ["(Enem 2018)  O colesterol total de uma pessoa é obtido pela soma da taxa do seu “colesterol bom“ com a taxa do seu “colesterol ruim”. Os exames periódicos, realizados em um paciente adulto, apresentaram taxa normal de 'colesterol bom', porém, taxa do 'colesterol ruim' (também chamado LDL) de O quadro apresenta uma classificação de acordo com as taxas de LDL em adultos. .:",
+        "ótima",  "próxima de ótima",  "limite",  "alta",  "muito alta",
+        "img/ex1.jpg", "", "","","","",
+        "4"
+    ],
+    ["(Enem 2018)  Devido ao não cumprimento das metas definidas para a campanha de vacinação contra a gripe comum e o vírus H1N1 em um ano, o Ministério da Saúde anunciou a prorrogação da campanha por mais uma semana. A tabela apresenta as quantidades de pessoas vacinadas dentre os cinco grupos de risco até a data de início da prorrogação da campanha. Qual é a porcentagem do total de pessoas desses grupos de risco já vacinadas? ",
+        "12",  "18",  "30",  "40",  "50",
+        "img/ex2.jpg","","","","","", 
+        "4"
     ],
 
+    ["(Enem 2018)  O gerente do setor de recursos humanos de uma empresa está organizando uma avaliação em que uma das etapas é um jogo de perguntas e respostas. Para essa etapa, ele classificou as perguntas, pelo nível de dificuldade, em fácil, médio e difícil, e escreveu cada pergunta em cartões para colocação em uma urna.Contudo, após depositar vinte perguntas de diferentes níveis na urna, ele observou que   deles eram de nível fácil. Querendo que as perguntas de nível fácil sejam a maioria, o gerente decidiu acrescentar mais perguntas de nível fácil à urna, de modo que a probabilidade de o primeiro participante retirar, aleatoriamente, uma pergunta de nível fácil seja de. Com essas informações, a quantidade de perguntas de nível fácil que o gerente deve acrescentar à urna é igual a " ,
+        "10",  "15",  "35",  "40",  "45",
+        "","", "","","","",
+         "4"
+
+
+    ],
+
+    ["(Enem 2018) A inclinação de uma rampa é calculada da seguinte maneira: para cada metro medido na horizontal, mede-se   centímetros na vertical. Diz-se, nesse caso, que a rampa tem inclinação de   como no exemplo da figura:" ,
+         "elevado em 40cm",   "elevado em 50cm",  "mantido no mesmo nível",  "rebaixado em 40cm", "rebaixado em 50cm" ,
+        "img/ex4.jpg", "","","","","",
+        "1"
+    
+
+    ],
+
+    ["(Enem 2018) Os alunos da disciplina de estatística, em um curso universitário, realizam quatro avaliações por semestre com os pesos de e respectivamente. No final do semestre, precisam obter uma média nas quatro avaliações de, no mínimo, pontos para serem aprovados. Um estudante dessa disciplina obteve os seguintes pontos nas três primeiras avaliações:e respectivamente. O mínimo de pontos que esse estudante precisa obter na quarta avaliação para ser aprovado é",
+        "29,8","71,0","74,5","75,5","84,0",
+        "", "", "","","","",
+        "3"
+    ],
+
+    ["(Enem 2016)  A fim de acompanhar o crescimento de crianças, foram criadas pela Organização Mundial da Saúde (OMS) tabelas de altura, também adotadas pelo Ministério da Saúde do Brasil. Além de informar os dados referentes ao índice de crescimento, a tabela traz gráficos com curvas, apresentando padrões de crescimento estipulados pela OMS. O gráfico apresenta o crescimento de meninas, cuja análise se dá pelo ponto de intersecção entre o comprimento, em centímetro, e a idade, em mês completo e ano, da criança. ",                               
+        "23,5%","21,2%","19,0%","11,8%","10,0%",
+        "img/ex6.jpg","", "","","","",
+        "1"
+    
+    ],
+
+    ["(Enem 2016)  O LlRAa, Levantamento Rápido do Índice de Infestação por Aedes aegypti, consiste num mapeamento da infestação do mosquito Aedes aegypti. O LlRAa é dado pelo percentual do número de imóveis com focos do mosquito, entre os escolhidos de uma região em avaliação. O serviço de vigilância sanitária de um município, no mês de outubro do ano corrente, analisou o LlRAa de cinco bairros que apresentaram o maior índice de infestação no ano anterior. Os dados obtidos para cada bairro foram:",
+        "I.","II.","III.","IV.","V.",
+        "img/ex7.jpg","", "","","","",
+        "1"
+    ],
+
+    ["(Enem 2016)  O censo demográfico é um levantamento estatístico que permite a coleta de várias informações. A tabela apresenta os dados obtidos pelo censo demográfico brasileiro nos anos de 1940 e 2000, referentes à concentração da população total, na capital e no interior, nas cinco grandes regiões.",
+        "125%","231%","331%","700%","800%",
+        "img/ex8.jpg","", "","","","",
+        "4"
+
+    ],
+
+    ["(Enem 2014)  Uma pessoa compra semanalmente, numa mesma loja, sempre a mesma quantidade de um produto que custa   a unidade. Como já sabe quanto deve gastar, leva sempre   a mais do que a quantia necessária para comprar tal quantidade, para o caso de eventuais despesas extras. Entretanto, um dia, ao chegar à loja, foi informada de que o preço daquele produto havia aumentado   Devido a esse reajuste, concluiu que o dinheiro levado era a quantia exata para comprar duas unidades a menos em relação à quantidade habitualmente comprada. A quantia que essa pessoa levava semanalmente para fazer a compra era ",
+        "R$166,00","R$156,00","R$84,00","R$46,00","R$24,00",
+        "","", "","","","",
+        "2"
+
+    ],
+
+    ["(Enem 2014)  O Brasil é um país com uma vantagem econômica clara no terreno dos recursos naturais, dispondo de uma das maiores áreas com vocação agrícola do mundo. Especialistas calculam que, dos   de hectares do país, as cidades, as reservas indígenas e as áreas de preservação, incluindo florestas e mananciais, cubram por volta de   de hectares. Aproximadamente   se destinam à agropecuária,   para pastagens e   para a agricultura, somadas as lavouras anuais e as perenes, como o café e a fruticultura. FORTES, G. “Recuperação de pastagens é alternativa para ampliar cultivos”. Folha de S. Paulo, 30 out. 2011. De acordo com os dados apresentados, o percentual correspondente à área utilizada para agricultura em relação à área do território brasileiro é mais próximo de",
+    "32,8%","28,6%","10,7%","9,4%","8,0%",
+    "","", "","","","",
+    "4"
+
+    ],
+
+
+
 ]
+                                                                        
